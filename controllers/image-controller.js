74e5ed743da542d98a4c95cf724e162628dc0c45
mikkomajamaa <mikko.majamaa@outlook.com>
2020-08-23 09:08:11 +0300

    refactor image-controller

--- controllers/image-controller.js
+++ controllers/image-controller.js
@@ -4,9 +4,8 @@ const helpers = require("./helpers/index");
 
 module.exports = {
   getImages: async (req, res, next) => {
-    let images = await Image.find().sort({ so: 0 }).populate("gallery");
-
     try {
+      let images = await Image.find().sort({ so: 0 }).populate("gallery");
       return res.status(200).json(images);
     } catch (error) {
       return res.status(501).json({ message: "Error getting images" });
@@ -58,27 +57,25 @@ module.exports = {
 
   putImages: async (req, res, next) => {
     const images = req.body.images;
-    for (let i = 0; i < images.length; i++) {
-      Image.update(
-        { _id: images[i]._id },
-        {
-          so: images[i].so,
-          alt_fi: images[i].alt_fi,
-          alt_en: images[i].alt_en,
-        },
-        (err, doc) => {
-          if (err) {
-            return res
-              .status(501)
-              .json({ message: "Error on saving record to database." });
+    try {
+      for (let i = 0; i < images.length; i++) {
+        await Image.update(
+          { _id: images[i]._id },
+          {
+            so: images[i].so,
+            alt_fi: images[i].alt_fi,
+            alt_en: images[i].alt_en,
+            gallery: images[i].gallery,
           }
-        }
-      );
+        );
+      }
+      let docs = await Gallery.find().sort({ so: 0 }).populate("images");
+      helpers.uploadSubGalleryJson(docs);
+      return res.status(200).json({ message: "success" });
+    } catch (error) {
+      return res
+        .status(501)
+        .json({ message: "Error on saving record to database." });
     }
-
-    let docs = await Gallery.find().sort({ so: 0 }).populate("images");
-    helpers.uploadSubGalleryJson(docs);
-
-    return res.status(200).json({ message: "success" });
   },
 };
