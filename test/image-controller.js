b054e0dd0751a782fff8ff5d2511faeaec333481
mikkomajamaa <mikko.majamaa@outlook.com>
2020-09-13 16:24:57 +0300

    add a test to image controller

--- test/image-controller.js
+++ test/image-controller.js
@@ -1,7 +1,34 @@
-const chai = require("chai").expect;
+const sinon = require("sinon");
+const chai = require("chai");
+const expect = chai.expect;
 
-describe("GET images route", () => {
-    it("Should get all the images", () => {
+const ImageController = require("../controllers/image-controller")
+const Image = require("../models/images");
 
+describe("ImageController", () => {
+    describe("GET images route", () => {
+        afterEach(() => {
+            sinon.restore();
+        })
+    
+        it("should get all the images", () => {
+            sinon.stub(Image, "find").returns([undefined]);
+            const res = {
+                images: [],
+                resCode: "",
+                json: function(images) {
+                    this.images = images;
+                    return this
+                },
+                status: function (res) {
+                    this.resCode = res;
+                    return this;
+                }
+            }
+            
+            ImageController.getImages({}, res, {});
+    
+            expect(res.images[0]).to.be.equal(undefined);
+        });
     });
-});
+}) 
